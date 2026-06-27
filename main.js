131b27a949a9fc780935271672b66299f3bbf9bf
Niverstan <strepoomich27@gmail.com>
2026-06-27 22:51:13 +0300

    Update and rename app.js to main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector(".download").onclick=()=>{
+
+window.location.href="download.html";
+
+}
+
+document.querySelector(".download2").onclick=()=>{
+
+window.location.href="download.html";
+
+}
